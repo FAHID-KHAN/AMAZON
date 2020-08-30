61afb5345dae6e3f32758ab205cf108090364042
FAHID_KHAN <fahidkhan1999.al@gmail.com>
2020-08-31 02:33:59 +0300

    Product Cpage component update

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import "./Product.css"
+import React from "react";
+import "./Product.css";
 
-function product(props) {
+function product({ id, title, price, rating, image }) {
   return (
-    <div>
-      
+    <div className="product">
+      <div className="product_info">
+        <p>{title}</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product_rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+
+      <img src={image} alt="" />
+      <button>Add to basket</button>
     </div>
-  )
+  );
 }
 
-export default product
+export default product;
